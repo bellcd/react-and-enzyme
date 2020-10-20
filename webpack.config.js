9bf9c3c83e2b8ed7fb3ff95d98ef47ae426c7d9f
Christian Bell <ChristianDibalaBell@gmail.com>
2020-10-20 16:15:58 -0400

    Adds setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js$|\.jsx$)/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
